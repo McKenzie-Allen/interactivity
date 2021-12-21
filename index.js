3c39092a53e5098a95aa45b54de84f6da80d10a7
McKenzie Allen <mckenzie.allen144@gmail.com>
2021-12-20 20:04:56 -0700

    starting to add the crossoffmovie function

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log("hello world")
+const message = document.querySelector('#message')
+
+function addMovie(event) {
+    event.preventDefault()
+    let inputField = document.querySelector('input')
+
+    const movie = document.createElement('li')
+
+    const movieTitle = document.createElement("span")
+    movieTitle.textContent = inputField.value
+    movie.appendChild(movieTitle)
+    document.querySelector('ul').appendChild(movie)
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'x'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+
+    inputField.value = ' '
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+}
+
+function crossOffMovie()
+
+document.querySelector('form').addEventListener('submit', addMovie)
